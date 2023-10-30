f60a083b09cf353e56d4f52dcd056d428c9653f6
stephennickalls <77465581+stephennickalls@users.noreply.github.com>
2023-10-30 14:09:20 +1300

    Update ApiaryTestHub-esp32.ino
    Add POST request method

--- ApiaryTestHub-esp32.ino
+++ ApiaryTestHub-esp32.ino
@@ -24,6 +24,30 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+void sendPostRequest() {
+    HTTPClient http;
+
+    // Your JSON data
+    String jsonData = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
+
+    http.begin(serverName); // Specify the URL
+    http.addHeader("Content-Type", "application/json"); // Specify content-type header
+
+    int httpResponseCode = http.POST(jsonData); // Send the actual POST request
+
+    if (httpResponseCode > 0) {
+        // If response, print out the response to the serial port
+        String response = http.getString();
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+    } else {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end(); // Free resources
+}
+
 void loop() {
-  // Nothing to do here
+
 }
